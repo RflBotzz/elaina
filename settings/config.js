c9a47dbd1fe99d57d9719ad7ce4d56de82e54ee5
RflBotzz <akunbotnew38@gmail.com>
2022-03-10 19:43:56 +0700

    Elaina

---
+++ settings/config.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// self or public
+global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+
+// setting
+global.ownername ="RafliKunn"
+global.ownernumber = "62895410068780"
+global.botname = "RflBotz Ofc"
+global.thumbnail = fs.readFileSync("./image/elaina.jpg") //sesuaikan dengan nama foto
+global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
+global.lolkey = 'rafliofcheh' //ganti pakai apikey kalian
+global.limit = {
+		free:20,
+		premium:1000
+	}
+global.session_name = "session.json"
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
